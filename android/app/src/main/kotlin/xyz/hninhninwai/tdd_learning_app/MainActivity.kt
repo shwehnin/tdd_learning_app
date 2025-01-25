293082d5edbaa64ab264d2d76a573600f94a4c39
shwehnin <hninhnin6398@gmail.com>
2025-01-25 23:02:52 +0700

    starter code for tdd learning app

---
+++ android/app/src/main/kotlin/xyz/hninhninwai/tdd_learning_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package xyz.hninhninwai.tdd_learning_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/authentication/domain/entities/user.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  final int id;
+  final String name;
+  final String avatar;
+  final String createdAt;
+
+  const User(
+      {required this.id,
+      required this.name,
+      required this.avatar,
+      required this.createdAt});
+
+  @override
+  List<Object?> get props => [id];
+}
